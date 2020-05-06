784746455bf6e1579eb02d36bc4390ca08e85eeb
YoshidaTatsumi <yoichi_1308@yahoo.co.jp>
2020-05-06 15:05:27 +0900

    [add]application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::Base
+	before_action :configure_permitted_parameters, if: :devise_controller?
+
+	def after_sign_in_path_for(resource)
+		case resource
+		when Admin
+			 admins_top_path
+		when User
+			 root_path
+		end
+	end
+
+	def after_sign_out_path_for(resource)
+		if resource == :admin
+      		new_admin_session_path
+    	elsif  resource == :user
+      		root_path
+    	end
+	end
 end
