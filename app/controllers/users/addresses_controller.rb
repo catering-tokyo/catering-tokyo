6f73888bb7fe7ba6201cdc8224dc9c27e4a68e64
YoshidaTatsumi <yoichi_1308@yahoo.co.jp>
2020-05-06 16:33:48 +0900

    [Add]address作成、削除機能

--- app/controllers/users/addresses_controller.rb
+++ app/controllers/users/addresses_controller.rb
@@ -1,18 +1,38 @@
 class Users::AddressesController < ApplicationController
   def index
     @address = Address.new
-    @addresses = Address.where(id: params[:id])
+    @addresses = Address.where(user_id: current_user.id)
   end
 
   def edit
+    @address = Address.find(params[:id])
   end
 
   def create
+    @address = Address.new(address_params)
+    @address.user_id = current_user.id
+    if  @address.save
+      flash[:success] = "新しい配送先を作成しました"
+      redirect_to users_addresses_path
+    else
+      flash.now[:danger] = "エラーです"
+      @addresses = Address.where(user_id: current_user.id)
+      render "index"
+    end
   end
 
   def update
   end
 
   def destroy
+    address = Address.find(params[:id])
+    address.destroy
+    flash[:success] = "１件の配送先を削除しました"
+    redirect_to users_addresses_path
+  end
+
+  private
+  def address_params
+    params.require(:address).permit(:name, :postal_code, :address, :phone_number)
   end
 end
--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,11 @@
 class Address < ApplicationRecord
 	belongs_to :user
+
+	validates :name, presence: true
+	validates :postal_code, presence: true
+	validates :address, presence: true
+	VALID_PHONE_NUMBER_REGEX = /\d{2,5}[-(]?\d{1,4}[-)]?\d{4}/
+	validates :phone_number, presence: true,
+                    		format: { with: VALID_PHONE_NUMBER_REGEX }
+
 end
