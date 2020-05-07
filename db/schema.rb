c1a440ff94b425948b91667345f49049e7aad444
masaki4200 <masa326420.4200@gmail.com>
2020-05-07 09:48:24 +0000

    [add]genre

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_03_044618) do
+ActiveRecord::Schema.define(version: 2020_05_07_093443) do
 
   create_table "addresses", force: :cascade do |t|
     t.integer "user_id", null: false
@@ -58,6 +58,13 @@ ActiveRecord::Schema.define(version: 2020_05_03_044618) do
   create_table "favorites", force: :cascade do |t|
     t.integer "user_id", null: false
     t.integer "shop_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "genres", force: :cascade do |t|
+    t.integer "shop_id", null: false
+    t.integer "shop_genreid", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -153,7 +160,6 @@ ActiveRecord::Schema.define(version: 2020_05_03_044618) do
     t.string "address", null: false
     t.string "shop_image_id"
     t.string "postal_code", null: false
-    t.integer "shop_genre_id", null: false
     t.string "open_time", null: false
     t.string "close_time", null: false
     t.string "lunch_limit"
