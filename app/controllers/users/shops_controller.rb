70c79f3692fd512f8e3cce506852aa6df9318378
vagrant <vagrant@localhost.localdomain>
2020-05-09 06:04:17 +0000

    shops

--- app/controllers/users/shops_controller.rb
+++ app/controllers/users/shops_controller.rb
@@ -1,18 +1,10 @@
 class Users::ShopsController < ApplicationController
-  def index
-<<<<<<< Updated upstream
-  	 	
+  def index 	 	
   end
 
   def show
   	@shop = Shop.find(params[:id])
-  	@reviews = Reviews.where(shop_id: params[:id])
-=======
-    @shops = Shop.all
-  end
-
-  def show
-    @shop = Shop.find(params[:id])
->>>>>>> Stashed changes
+  	@review = Review.new
+  	@reviews = @shop.reviews
   end
 end
