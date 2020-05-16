234319aa41a25a1bb9f15a4861b50f9094ff71ef
masahiro-ya <abcabcmart5644@gmail.com>
2020-05-16 04:53:23 +0000

    admin問い合わせ完了

--- app/mailers/inquiries_mailer.rb
+++ app/mailers/inquiries_mailer.rb
@@ -5,4 +5,10 @@ class InquiriesMailer < ApplicationMailer
     @inquiry = inquiry
     mail to: @shop.email, subject: '【Catering Tokyo】 お問い合わせありがとうございます'
   end
+
+  def admin_replied(shop,inquiry)
+    @shop = shop #ショップー情報
+    @inquiry = inquiry
+    mail to: @shop.email, subject: '【Catering Tokyo】 お問い合わせの件について'
+  end
 end
