e9ae1d8a0a6e045bd990fae7cad8abcc26139f77
masahiro-ya <abcabcmart5644@gmail.com>
2020-05-19 08:09:00 +0000

    チャット

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,7 +1,5 @@
 class Message < ApplicationRecord
   validates :message, presence: true
-  # createの後にコミットする { MessageBroadcastJobのperformを遅延実行 引数はself }
-  after_create_commit { MessageBroadcastJob.perform_later self }
 	belongs_to :user, optional: true
 	belongs_to :shop, optional: true
 	belongs_to :room
