ca155f3c94b3fda18f5259f621769d7518e5128e
masahiro-ya <abcabcmart5644@gmail.com>
2020-05-21 09:04:29 +0000

    チャット実装

--- app/controllers/users/homes_controller.rb
+++ app/controllers/users/homes_controller.rb
@@ -1,7 +1,15 @@
 class Users::HomesController < ApplicationController
   def top
+  	@search_params = user_search_params
+    @users = User.search(@search_params).includes(:prefecture)
   end
 
   def about
+  end
+
+  private
+
+  def user_search_params
+    params.fetch(:search, {}).permit(:name, :gender, :birthday_from, :birthday_to, :prefecture_id)
   end
 end
